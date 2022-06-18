42ee13ad8dea20d4ac1ac986aaa12c55d3e42dc0
Vivek R <vivekkravindraa@gmail.com>
2022-06-18 22:23:09 +0530

    eslint-disable-nex-line

--- src/features/posts/PostsView.js
+++ src/features/posts/PostsView.js
@@ -33,6 +33,7 @@ export default function PostsView() {
 
   useEffect(() => {
     dispatch(fetchPosts());
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const displayFormModal = () => {
